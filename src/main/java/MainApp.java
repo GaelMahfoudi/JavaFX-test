7b28d8bb968f274a5d807d224b59bab208dcf78d
Gaël <gael.mahfoudi@gmail.com>
2016-04-20 13:00:05 +0200

    Add Main

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Gaël on 20/04/2016.
+ */
+public class MainApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Test");
+
+        primaryStage.show();
+    }
+}
